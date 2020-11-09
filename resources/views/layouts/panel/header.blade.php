f4b1717e18eb63f5582d9aabef09626a47356249
m_boluki <mr.boluki@gmail.com>
2020-11-09 18:51:48 +0330

    دکمه های آپلود فایل کار میکنند باگ : دو تا دکمه توی صفحه داریم

--- resources/views/layouts/panel/header.blade.php
+++ resources/views/layouts/panel/header.blade.php
@@ -1,22 +1,42 @@
-<div class="bg-dark new-header text-light p-4">
-        <div class="d-flex">
-            <h3>
-                {{ env('APP_NAME') }}
-            </h3>
-            <small title=" {{ $Version[1] }} " class="eng text-warning">
-               ({{  $Version[0]  }})
-            </small>
-        </div>
-        <div class="version">
-            <small class="small badge badge-pill badge-warning eng ">
-                <?php echo trim(exec('git describe --tags --abbrev=3')); ?>
-            </small>
-            <small class="small" dir="ltr">
-                <?php 
-                    $gd = trim(exec('git --no-pager log -1 --format="%ai"'));
-                    echo \Morilog\Jalali\Jalalian::forge($gd);
-                ?>
-                
-            </small>
-        </div>
+<div class="stuff-header">
+    <div>
+        <a href="#"
+        class="btn btn-success"
+        id="insert-new-stuff-button"
+        data-user-id="{{ Auth::id() }}">
+            <i class="fas fa-plus"></i>
+            کالای جدید
+        </a>
+        <form id="form" action = {{route('uploadStuff')}} method="post" class="d-inline" enctype="multipart/form-data">
+        @csrf
+            <label
+            for="file-form-1"
+        class="btn btn-info text-light"
+        id="insert-new-stuff-file-button">
+            <i class="fas fa-file-upload "></i>
+            ارسال از طریق فایل
+        </label>
+        <input type="file" id="file-form-1" style="display: none" name="file">
+        </form>
+    </div>
+
+    {{-- <table class="table table-striped table-bordered " id="stuffs-table">
+        <thead>
+            <tr class="table-primary">
+                <th>ردیف</th>
+                <th>کد کالا</th>
+                <th>نام کالا</th>
+                <th>نام لاتین</th>
+                <th>سریال منحصر بفرد</th>
+                <th>واحد اندازه‌گیری</th>
+                <th>توضیحات</th>
+                <th>عملیات</th>
+            </tr>
+        </thead> --}}
 </div>
+<script>
+
+    document.getElementById("file-form-1").onchange = function() {
+        document.getElementById("form").submit();
+    };
+</script>
