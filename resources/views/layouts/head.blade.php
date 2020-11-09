e9da7cd5d3711a4b34766839a307e9c3661e884b
mrJAVAD <miniopera165@gmail.com>
2020-11-09 17:14:06 +0330

    using csv for saving data

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <head>
     <meta charset="utf-8">
@@ -87,6 +88,7 @@
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
     <!-- Scripts -->
+    <script src="{{asset('js/uploader.min.js')}}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     @yield('js')
 </body>
