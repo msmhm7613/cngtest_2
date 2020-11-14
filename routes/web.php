32997b0d837d7d5cf1ba96d35ca678ae352a79c1
Kia <kia.boluki@gmail.com>
2020-11-14 18:39:54 +0330

    conflict resolved

--- routes/web.php
+++ routes/web.php
@@ -117,3 +117,11 @@ Route::post('workshop/create', 'App\Http\Controllers\tempstore\TempstoreControll
 Route::post('transfer/create','App\Http\Controllers\transfer\TransferController@insert')->name('createTransfer');
 
 Route::get('check_serial/{reciept_no}','App\Http\Controllers\stuff\stuffController@check_serial');
+
+
+Route::post('carEdit',[carController::class,'edit']);
+Route::post('CarFile',[carController::class,'upload'])->name('carUpload');
+Route::post('CarInsert',[carController::class,'insert'])->name('carInsert');
+Route::post('carEdit',[carController::class,'edit']);
+Route::post('carUpdate',[carController::class,'update'])->name('carUpdate');
+
